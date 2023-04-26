52605b45f7edc904baf5a66081ca8b49522e3ac1
tomer-shavit <tomer.shavit5@gmail.com>
2023-04-26 15:28:22 +0300

    added animations to PostUplaod

--- src/components/PostUpload.tsx
+++ src/components/PostUpload.tsx
@@ -12,11 +12,32 @@ const PostUpload: React.FC<{ file: File; data: GptApiResponse }> = ({
   return (
     <div className="flex  ml-10 flex-row justify-evenly items-center">
       <div className="w-1/2 flex-col justify-between">
-        <SummaryView data={data}></SummaryView>
-        <SuggestionsView data={data}></SuggestionsView>
+        <m.div
+          initial={{ opacity: 0, y: "5%" }}
+          animate={{ opacity: 1, y: "0%" }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          exit={{ opacity: 0, y: "5%" }}
+        >
+          <SummaryView data={data}></SummaryView>
+        </m.div>
+        <m.div
+          initial={{ opacity: 0, y: "5%" }}
+          animate={{ opacity: 1, y: "0%" }}
+          transition={{ duration: 0.5, ease: "easeOut", delay: 0.25 }}
+          exit={{ opacity: 0, y: "5%" }}
+        >
+          <SuggestionsView data={data}></SuggestionsView>
+        </m.div>
       </div>
       <div className="w-1/2 flex flex-row justify-center">
-        <AnalysisView data={data}></AnalysisView>
+        <m.div
+          initial={{ opacity: 0, y: "5%" }}
+          animate={{ opacity: 1, y: "0%" }}
+          transition={{ duration: 0.5, ease: "easeOut", delay: 0.15 }}
+          exit={{ opacity: 0, y: "5%" }}
+        >
+          <AnalysisView data={data}></AnalysisView>
+        </m.div>
       </div>
     </div>
   );
