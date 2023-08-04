8c5e4470c6b3e4e58c0364e735aa4feefbbc3a4f
tomer-shavit <tomer.shavit5@gmail.com>
2023-08-04 19:23:57 +0300

    added error page and bug form

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,12 @@ const NavBar: React.FC = (props) => {
     <nav className="flex items-center justify-between flex-wrap bg-transparent p-4">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link href="/">
-          <span className="font-semibold text-2xl tracking-tight">CVBoost</span>
+          <span
+            className="font-semibold text-2xl tracking-tight"
+            onClick={() => window.location.reload()}
+          >
+            CVBoost
+          </span>
         </Link>
       </div>
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
---
+++ src/components/SomethingWentWrong.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { motion as m } from "framer-motion";
+
+const FORM = "https://forms.gle/tmhduogmZfmcdm9d9";
+
+const SomethingWentWrong: React.FC = () => {
+  const openLinkInNewTab = (url: string) => {
+    window.open(url, "_blank");
+    window.location.reload();
+  };
+  return (
+    <div className="flex flex-col items-center p-4 w-3/4">
+      <m.div
+        initial={{ opacity: 0, y: "5%" }}
+        animate={{ opacity: 1, y: "0%" }}
+        transition={{ duration: 0.75, ease: "easeOut" }}
+        exit={{ opacity: 0, y: "5%" }}
+      >
+        <Image
+          src="/images/anxiety.png"
+          width={250}
+          height={250}
+          alt="sad face"
+          className="mb-6"
+        ></Image>
+      </m.div>
+      <m.div
+        initial={{ opacity: 0, y: "5%" }}
+        animate={{ opacity: 1, y: "0%" }}
+        transition={{ duration: 0.75, ease: "easeOut" }}
+        exit={{ opacity: 0, y: "10%" }}
+        className="flex flex-col items-center"
+      >
+        <p className="text-white text-2xl  mt-4">
+          Oops.. Somehting went wrong on our side.
+        </p>
+        <p className="text-white text-2xl  mt-2">
+          Fill this form to help us imporve:
+        </p>
+        <a
+          className="text-green-400 hover:green-600 text-2xl mt-2 font-semibold cursor-pointer"
+          onClick={() => openLinkInNewTab(FORM)}
+        >
+          Click Here
+        </a>
+      </m.div>
+    </div>
+  );
+};
+
+export default SomethingWentWrong;
