27993fd20242834a9b40308983ce068df63ea679
tomer-shavit <tomer.shavit5@gmail.com>
2023-08-13 20:27:23 +0300

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className="overscroll-none overflow-x-none selection:bg-green-600 scrollbar-thin scrollbar-track-slate-700 scroll scrollbar-thumb-rounded scrollbar-thumb-slate-800">
         <Main />
         <NextScript />
