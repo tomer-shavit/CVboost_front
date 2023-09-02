4e08813c102ee02531d0c3e356bd1edbca8f4486
tomer-shavit <tomer.shavit5@gmail.com>
2023-09-02 22:21:06 +0300

    removed footer from clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,13 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          footer: "hidden",
+        },
+      }}
+    >
       <Component {...pageProps} />
     </ClerkProvider>
   );
