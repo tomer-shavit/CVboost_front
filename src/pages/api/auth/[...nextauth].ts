afc7a0ef6f98f7c8fde8058bc90f05c2b8f7e91d
tomer-shavit <tomer.shavit5@gmail.com>
2023-09-14 18:08:01 +0300

    added id to session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,6 @@
-import NextAuth from "next-auth";
+import NextAuth, { SessionStrategy } from "next-auth";
+import { Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "../../../../prisma/client";
 import planetScaleAdapter from "@/adapters/planetScale";
@@ -11,6 +13,17 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET ?? "MISSING_SECRET",
     }),
   ],
+  callbacks: {
+    session: async ({ session, token }: { session: Session; token: JWT }) => {
+      if (session?.user) {
+        session.user.id = token.sub ?? "NO_TOKEN_SUB";
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt" as SessionStrategy,
+  },
 };
 
 export default NextAuth(authOptions);
