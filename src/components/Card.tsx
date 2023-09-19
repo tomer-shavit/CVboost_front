98cc62c713cfa7c27ea00c1123d85909ce2fca5b
tomer-shavit <tomershav@wix.com>
2023-09-19 10:10:07 +0300

    changed shadow to lg on Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,7 @@
 const Card: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="h-full flex flex-col justify-center">
-      <div className="rounded-lg shadow-md bg-slate-700 p-6 overflow-y-auto scrollbar-thin scroll scrollbar-thumb-rounded scrollbar-thumb-slate-800 ">
+      <div className="rounded-lg shadow-lg bg-slate-700 p-6 overflow-y-auto scrollbar-thin scroll scrollbar-thumb-rounded scrollbar-thumb-slate-800 ">
         {children}
       </div>
     </div>
